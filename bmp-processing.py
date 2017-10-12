7d2274e17bbf660ffbf303c6447e1aeae41360d7
cs13082 <honda.toshiki.17@shizuoka.ac.jp>
2017-10-12 15:25:18 +0900

    Cleanup codes & Edited README.md

--- bmp-processing.py
+++ bmp-processing.py
@@ -1,6 +1,9 @@
-# coding: UTF-8
+""" This program converts ./gazou1.bmp to ./gazou2.bmp """
+# -*- coding: UTF-8 -*-
 # Tested on Python 3.6.3 (32bit)
-# This program converts ./gazou1.bmp to ./gazou2.bmp
+
+READPATH = r"./gazou1.bmp"
+WRITEPATH = r"./gazou2.bmp"
 
 class BmpFile:
     """
@@ -23,129 +26,131 @@ class BmpFile:
     4byte impcolornum 重要色数
     """
     def __init__(self):
-        self.idchara = bytes()
-        self.filesize = bytes()
-        self.res_a = bytes()
-        self.res_b = bytes()
-        self.headersize = bytes()
-        self.infosize = bytes()
-        self.width = bytes()
-        self.height = bytes()
-        self.pixnum = bytes()
-        self.colordepth = bytes()
-        self.compmethod = bytes()
-        self.compsize = bytes()
-        self.horizonalres = bytes()
-        self.verticalres = bytes()
-        self.colornum = bytes()
-        self.impcolornum = bytes()
-        self.color_r = []
-        self.color_g = []
-        self.color_b = []
-        self.w = int()
-        self.h = int()
+        self.__idchara = bytes()
+        self.__filesize = bytes()
+        self.__res_a = bytes()
+        self.__res_b = bytes()
+        self.__headersize = bytes()
+        self.__infosize = bytes()
+        self.__width = bytes()
+        self.__height = bytes()
+        self.__pixnum = bytes()
+        self.__colordepth = bytes()
+        self.__compmethod = bytes()
+        self.__compsize = bytes()
+        self.__horizonalres = bytes()
+        self.__verticalres = bytes()
+        self.__colornum = bytes()
+        self.__impcolornum = bytes()
+        self.__color_r = []
+        self.__color_g = []
+        self.__color_b = []
 
     def read(self, bmpfile):
-        self.idchara = bmpfile.read(2)
-        self.filesize = bmpfile.read(4)
-        self.res_a = bmpfile.read(2)
-        self.res_b = bmpfile.read(2)
-        self.headersize = bmpfile.read(4)
-        self.infosize = bmpfile.read(4)
-        self.width = bmpfile.read(4)
-        self.height = bmpfile.read(4)
-        self.pixnum = bmpfile.read(2)
-        self.colordepth = bmpfile.read(2)
-        self.compmethod = bmpfile.read(4)
-        self.compsize = bmpfile.read(4)
-        self.horizonalres = bmpfile.read(4)
-        self.verticalres = bmpfile.read(4)
-        self.colornum = bmpfile.read(4)
-        self.impcolornum = bmpfile.read(4)
-        
-        self.w = int.from_bytes(self.width, 'little')
-        self.h = int.from_bytes(self.height, 'little')
-        
+        self.__idchara = bmpfile.read(2)
+        self.__filesize = bmpfile.read(4)
+        self.__res_a = bmpfile.read(2)
+        self.__res_b = bmpfile.read(2)
+        self.__headersize = bmpfile.read(4)
+        self.__infosize = bmpfile.read(4)
+        self.__width = bmpfile.read(4)
+        self.__height = bmpfile.read(4)
+        self.__pixnum = bmpfile.read(2)
+        self.__colordepth = bmpfile.read(2)
+        self.__compmethod = bmpfile.read(4)
+        self.__compsize = bmpfile.read(4)
+        self.__horizonalres = bmpfile.read(4)
+        self.__verticalres = bmpfile.read(4)
+        self.__colornum = bmpfile.read(4)
+        self.__impcolornum = bmpfile.read(4)
+
         tmp = 0
-        while 1 :
+        while 1:
             readtmp = bmpfile.read(1)
+            # ファイル終了時break
             if readtmp == b'':
                 break
             readtmpint = int.from_bytes(readtmp, 'little')
             if tmp == 0:
-                self.color_b.append(readtmpint)
+                self.__color_b.append(readtmpint)
                 tmp += 1
             elif tmp == 1:
-                self.color_g.append(readtmpint)
+                self.__color_g.append(readtmpint)
                 tmp += 1
             elif tmp == 2:
-                self.color_r.append(readtmpint)
+                self.__color_r.append(readtmpint)
                 tmp = 0
 
     def write(self, filepath):
         try:
             bmpdata = open(filepath, 'wb')
-            bmpdata.write(self.idchara)
-            bmpdata.write(self.filesize)
-            bmpdata.write(self.res_a)
-            bmpdata.write(self.res_b)
-            bmpdata.write(self.headersize)
-            bmpdata.write(self.infosize)
-            bmpdata.write(self.width)
-            bmpdata.write(self.height)
-            bmpdata.write(self.pixnum)
-            bmpdata.write(self.colordepth)
-            bmpdata.write(self.compmethod)
-            bmpdata.write(self.compsize)
-            bmpdata.write(self.horizonalres)
-            bmpdata.write(self.verticalres)
-            bmpdata.write(self.colornum)
-            bmpdata.write(self.impcolornum)
+            bmpdata.write(self.__idchara)
+            bmpdata.write(self.__filesize)
+            bmpdata.write(self.__res_a)
+            bmpdata.write(self.__res_b)
+            bmpdata.write(self.__headersize)
+            bmpdata.write(self.__infosize)
+            bmpdata.write(self.__width)
+            bmpdata.write(self.__height)
+            bmpdata.write(self.__pixnum)
+            bmpdata.write(self.__colordepth)
+            bmpdata.write(self.__compmethod)
+            bmpdata.write(self.__compsize)
+            bmpdata.write(self.__horizonalres)
+            bmpdata.write(self.__verticalres)
+            bmpdata.write(self.__colornum)
+            bmpdata.write(self.__impcolornum)
 
-            for i in range(len(self.color_b)):
-                bmpdata.write(self.color_b[i].to_bytes(1, 'little'))
-                bmpdata.write(self.color_g[i].to_bytes(1, 'little'))
-                bmpdata.write(self.color_r[i].to_bytes(1, 'little'))
+            for i in range(len(self.__color_b)):
+                bmpdata.write(self.__color_b[i].to_bytes(1, 'little'))
+                bmpdata.write(self.__color_g[i].to_bytes(1, 'little'))
+                bmpdata.write(self.__color_r[i].to_bytes(1, 'little'))
 
             bmpdata.close()
         except():
             print("OPEN ERROR")
             exit()
-    
-    def getArray(self):
-        arr_b = [[0 for i in range(self.h)] for j in range(self.w)]
-        arr_g = [[0 for i in range(self.h)] for j in range(self.w)]
-        arr_r = [[0 for i in range(self.h)] for j in range(self.w)]
-        for i in range(len(self.color_b)):
-            arr_b[i%self.w][i//self.w] = self.color_b[i]
-        for i in range(len(self.color_g)):
-            arr_g[i%self.w][i//self.w] = self.color_g[i]
-        for i in range(len(self.color_r)):
-            arr_r[i%self.w][i//self.w] = self.color_r[i]
-        return [arr_b, arr_g, arr_r]
 
-    def setArray(self, arr_b, arr_g, arr_r):
+    def getwidth(self):
+        """ Returns width by int """
+        return int.from_bytes(self.__width, 'little')
+
+    def getheight(self):
+        """ Returns height by int """
+        return int.from_bytes(self.__height, 'little')
+
+    def getBGR(self):
+        """ Returns arr[width][height][color(gbr)] """
+        arr = [[0 for i in range(self.getheight())] for j in range(self.getwidth())]
+        for i in range(len(self.__color_b)):
+            arr[i%self.getwidth()][i//self.getwidth()] = [self.__color_b[i], self.__color_g[i], self.__color_r[i]]
+        return arr
+
+    def setBGR(self, arr):
+        """ requires gbr array """
         i = 0
-        for he in range(self.h):
-            for wi in range(self.w):
-                self.color_b[i] = arr_b[wi][he]
-                self.color_g[i] = arr_g[wi][he]
-                self.color_r[i] = arr_r[wi][he]
+        for hei in range(self.getheight()):
+            for wid in range(self.getwidth()):
+                self.__color_b[i] = arr[wid][hei][0]
+                self.__color_g[i] = arr[wid][hei][1]
+                self.__color_r[i] = arr[wid][hei][2]
                 i += 1
 
-    def getEmptyArray(self):
-        return [[0 for i in range(self.h)] for j in range(self.w)]
-
-
 def main():
-    print("Convert ./gazou1.bmp to ./gazou2.bmp")
+    print("\nConvert {} to {}".format(READPATH, WRITEPATH))
     print("1 - Greyscale (Middle value)")
     print("2 - Greyscale (NTSC Coef. method)")
     print("3 - Color Inversion")
     print("4 - Blur")
-    keyinput = input("Please type the number: ")
-    
-    bmpdata = readBMP(r"./gazou1.bmp")
+    keyinput = input("\nPlease type the number: ")
+
+    bmpdata = BmpFile()
+
+    # readBMP
+    bmpdata.read(open(READPATH, 'rb'))
+    print("Read the file")
+
+    # Image Processing
     if int(keyinput) == 1:
         convertToGreyscale1(bmpdata)
     elif int(keyinput) == 2:
@@ -154,93 +159,83 @@ def main():
         invertColors(bmpdata)
     elif int(keyinput) == 4:
         blurImage(bmpdata)
-    writeBMP(r"./gazou2.bmp", bmpdata)
-    
 
-def readBMP(filepath):
-    bmpdata = BmpFile()
-    bmpdata.read(open(filepath, 'rb'))
-    print("Read the file")
-
-    return bmpdata
-
-
-def writeBMP(filepath, bmpdata):
-    bmpdata.write(filepath)
+    # writeBMP
+    bmpdata.write(WRITEPATH)
     print("Wrote the file")
-    return
 
 
 def convertToGreyscale1(bmpdata):
-    # middle value
-    for i in range(len(bmpdata.color_b)):
-        bmax = max(bmpdata.color_b[i], bmpdata.color_g[i], bmpdata.color_r[i])
-        bmin = min(bmpdata.color_b[i], bmpdata.color_g[i], bmpdata.color_r[i])
-        bmpdata.color_b[i] = bmpdata.color_g[i] = bmpdata.color_r[i] = ( bmax + bmin ) // 2
+    """ middle value """
+    arr = bmpdata.getBGR()
+    for hei in range(bmpdata.getheight()):
+        for wid in range(bmpdata.getwidth()):
+            calc = (max(arr[wid][hei]) + min(arr[wid][hei])) // 2
+            arr[wid][hei] = [calc, calc, calc]
+    bmpdata.setBGR(arr)
     print("Greyscaled (Middle value) the file")
 
-
 def convertToGreyscale2(bmpdata):
-    # NTSC Coef. method
-    for i in range(len(bmpdata.color_b)):
-        bmpdata.color_b[i] = bmpdata.color_g[i] = bmpdata.color_r[i] = \
-            int(0.298912 * bmpdata.color_r[i] + 0.586611 * bmpdata.color_g[i] + 0.114478 * bmpdata.color_b[i])
+    """ NTSC Coef. method """
+    arr = bmpdata.getBGR()
+    for hei in range(bmpdata.getheight()):
+        for wid in range(bmpdata.getwidth()):
+            calc = int(0.298912*arr[wid][hei][2] + 0.586611*arr[wid][hei][1] + 0.114478*arr[wid][hei][0])
+            arr[wid][hei] = [calc, calc, calc]
+    bmpdata.setBGR(arr)
     print("Greyscaled (NTSC Coef. method) the file")
 
-
 def invertColors(bmpdata):
-    for i in range(len(bmpdata.color_b)):
-        # 念のため絶対値
-        bmpdata.color_b[i] = abs(255 - bmpdata.color_b[i])
-        bmpdata.color_g[i] = abs(255 - bmpdata.color_g[i])
-        bmpdata.color_r[i] = abs(255 - bmpdata.color_r[i])
+    arr = bmpdata.getBGR()
+    for hei in range(bmpdata.getheight()):
+        for wid in range(bmpdata.getwidth()):
+            arr[wid][hei] = [abs(255 - arr[wid][hei][0]),
+                             abs(255 - arr[wid][hei][1]),
+                             abs(255 - arr[wid][hei][2])]
+    bmpdata.setBGR(arr)
     print("Inverted the color")
 
-
 def blurImage(bmpdata):
-    #ぼかし
-    arr_b, arr_g, arr_r = bmpdata.getArray()
-    arr_b_mod = bmpdata.getEmptyArray()
-    arr_g_mod = bmpdata.getEmptyArray()
-    arr_r_mod = bmpdata.getEmptyArray()
-    width = bmpdata.w
-    height = bmpdata.h
-    for h in range(height):
-        for w in range(width):
-            sum_b = arr_b[w][h]
-            sum_g = arr_g[w][h]
-            sum_r = arr_r[w][h]
-            count = 1
-            # 周囲ピクセル範囲外の場合
-            if w - 1 >= 0:
-                sum_b += arr_b[w-1][h]
-                sum_g += arr_g[w-1][h]
-                sum_r += arr_r[w-1][h]
+    arr = bmpdata.getBGR()
+    arr_mod = getEmptyArray(bmpdata.getwidth(), bmpdata.getheight())
+    for hei in range(bmpdata.getheight()):
+        for wid in range(bmpdata.getwidth()):
+            sum_b = arr[wid][hei][0]
+            sum_g = arr[wid][hei][1]
+            sum_r = arr[wid][hei][2]
+            count = 1   # 割る数
+            # 周囲ピクセルが範囲外の場合に除外
+            if wid - 1 >= 0:
+                sum_b += arr[wid-1][hei][0]
+                sum_g += arr[wid-1][hei][1]
+                sum_r += arr[wid-1][hei][2]
                 count += 1
-            if w + 1 < width:
-                sum_b += arr_b[w+1][h]
-                sum_g += arr_g[w+1][h]
-                sum_r += arr_r[w+1][h]
+            if wid + 1 < bmpdata.getwidth():
+                sum_b += arr[wid+1][hei][0]
+                sum_g += arr[wid+1][hei][1]
+                sum_r += arr[wid+1][hei][2]
                 count += 1
-            if h - 1 >= 0:
-                sum_b += arr_b[w][h-1]
-                sum_g += arr_g[w][h-1]
-                sum_r += arr_r[w][h-1]
+            if hei - 1 >= 0:
+                sum_b += arr[wid][hei-1][0]
+                sum_g += arr[wid][hei-1][1]
+                sum_r += arr[wid][hei-1][2]
                 count += 1
-            if h + 1 < height:
-                sum_b += arr_b[w][h+1]
-                sum_g += arr_g[w][h+1]
-                sum_r += arr_r[w][h+1]
+            if hei + 1 < bmpdata.getheight():
+                sum_b += arr[wid][hei+1][0]
+                sum_g += arr[wid][hei+1][1]
+                sum_r += arr[wid][hei+1][2]
                 count += 1
-                
-            arr_b_mod[w][h] = (sum_b // count)
-            arr_g_mod[w][h] = (sum_g // count)
-            arr_r_mod[w][h] = (sum_r // count)
-            
-    bmpdata.setArray(arr_b_mod, arr_g_mod, arr_r_mod)
+
+            arr_mod[wid][hei] = [sum_b // count, sum_g // count, sum_r // count]
+
+    bmpdata.setBGR(arr_mod)
     print("Blured the image")
+
+
+def getEmptyArray(width, height):
+    return [[0 for i in range(height)] for j in range(width)]
 
 
 if __name__ == '__main__':
     main()
-    print("FINISHED")
+    print("FINISHED!\n")
